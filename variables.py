53adbc6dcc072f096c0f2ad96b6103e345eddd27
Marc Pérez Fusco <71872901+mapefu@users.noreply.github.com>
2025-11-15 13:42:04 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,19 +1,14 @@
 import pandas as pd
 
 # Leer CSV indicando que el separador es ;
-df = pd.read_csv('train.csv', sep=';')
+df = pd.read_csv('dades\\dades.csv', sep=';')
 
-df_sin_una = df.loc[:, df.columns != df.columns[8]]
-# Mostrar nombres de columnas correctamente
-#print(df.columns)
-
-# Seleccionar las dos primeras columnas
 col = pd.read_csv('train.csv', sep=';', usecols=[df.columns[0], df.columns[27], df.columns[-1]])
 
 families = []
 
-for n in df_sin_una.columns[2:]:
-    families.append(df_sin_una[n].unique())
+for n in df.columns[2:]:
+    families.append(df[n].unique())
 
 families.to_csv('families.csv', sep=';', index=False)
 
