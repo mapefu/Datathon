30dc29ff840b394f7e723a8dcedb08c455965368
Marc Pérez Fusco <71872901+mapefu@users.noreply.github.com>
2025-11-15 13:46:58 +0100

    famiies.csv

--- variables.py
+++ variables.py
@@ -3,13 +3,12 @@ import pandas as pd
 # Leer CSV indicando que el separador es ;
 df = pd.read_csv('dades\\dades.csv', sep=';')
 
-col = pd.read_csv('train.csv', sep=';', usecols=[df.columns[0], df.columns[27], df.columns[-1]])
-
 families = []
 
 for n in df.columns[2:]:
     families.append(df[n].unique())
 
-families.to_csv('families.csv', sep=';', index=False)
 
-print(families)
+f = pd.DataFrame(families)
+print(f)
+f.to_csv('dades\\families.csv', sep=';', index=False)
