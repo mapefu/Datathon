4ba103227c9fc19269cbf6af2ad8251a4141b33e
Marc Pérez Fusco <71872901+mapefu@users.noreply.github.com>
2025-11-15 22:39:49 +0100

    Update entrenament.py

--- entrenament.py
+++ entrenament.py
@@ -4,16 +4,17 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 
-df = pd.read_csv('data_train_procesada.csv', sep=';')
-dt = pd.read_csv('grouped_data_test.csv', sep=';')
+df = pd.read_csv('data_train_proces_temporadas.csv', sep=';')
+dt = pd.read_csv('test\\grouped_data_test_temporadas.csv', sep=';')
 
-X = df[list(df.columns[1:18])+ list(df.columns[20:-3])]
+X = df[list(df.columns[1:5])+list(df.columns[6:18])+ list(df.columns[20:-3])]
 y_train = df[df.columns[-1]]
-Xt = dt[list(dt.columns[1:18])+ list(dt.columns[20:-3])]
-y_test = dt[dt.columns[-1]]
+Xt = dt[list(dt.columns[1:5])+list(dt.columns[6:18])+ list(dt.columns[20:-3])]
 # Convertir las columnas categóricas a variables numéricas
 X_train = pd.get_dummies(X)
 X_test = pd.get_dummies(Xt)
+
+X_test_aligned = X_test.reindex(columns=X_train.columns, fill_value=0)
 
 print(X_train)
 
@@ -22,16 +23,20 @@ print(X_train)
 modelo = RandomForestRegressor()
 modelo.fit(X_train, y_train)
 
-predicciones = modelo.predict(X_test)
+predicciones = modelo.predict(X_test_aligned)
+
+predicciones_aumentadas = (predicciones * 1.11)
+# Redondear predicciones y convertir a enteros
+predicciones_int = np.round(predicciones_aumentadas).astype(int)
 
 # Crear DataFrame con la primera columna de Xt y y_test
 resultados = pd.DataFrame({
-    Xt.columns[0]: Xt.iloc[:, 0],  # Primera columna de Xt
-    'Production': y_test.values
+    'ID': dt.iloc[:, 0].values,
+    'Production': predicciones_int
 })
 
 # Guardar DataFrame a CSV
-resultados.to_csv('submition.csv', sep=';', index=False)
+resultados.to_csv('submition.csv', sep=',', index=False)
 
 '''
 # Crear DataFrame con resultados reales y predicciones
