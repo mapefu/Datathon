596d22dc1b34f8d879bee179e0df624575e3e80f
Marc Pérez Fusco <71872901+mapefu@users.noreply.github.com>
2025-11-15 17:28:13 +0100

    Update entrenament.py

--- entrenament.py
+++ entrenament.py
@@ -1,20 +1,33 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
 
-df = pd.read_csv('grouped_data.csv', sep=';')
+df = pd.read_csv('train.csv', sep=';')
 
-X = df[[df.columns[1], df.columns[3], df.columns[-2]]]
-y = df[df.columns[2]]
+X = df[list(df.columns[1:8]) + [df.columns[18], df.columns[9], df.columns[10]] + list(df.columns[20:28])]
+y = df[df.columns[-2]]
 
 # Convertir las columnas categóricas a variables numéricas
 X_encoded = pd.get_dummies(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2)
+print(X_encoded)
 
-modelo = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.1)
+
+modelo = RandomForestRegressor()
 modelo.fit(X_train, y_train)
 
 predicciones = modelo.predict(X_test)
 
-print(predicciones)
+# Crear DataFrame con resultados reales y predicciones
+resultados = pd.DataFrame({
+    'Valor Real': y_test.values,
+    'Predicción': predicciones
+})
+
+desviacion_tipica = np.std(resultados['Valor Real'] - resultados['Predicción'])
+print(f"Desviación típica entre predicción y valor real: {desviacion_tipica}")
+
+# Guardar en archivo CSV
+resultados.to_csv('predicciones_y_reales.csv', index=False, sep=';')
